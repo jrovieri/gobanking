6ff3937458316fe946edb8cb714230d20f000569
Jonathan Rovieri <jrovieri@gmail.com>
2025-07-07 20:40:32 -0300

    refactoring

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jrovieri/gobanking
+
+go 1.24.4
--- main.go
+++ main.go
@@ -1,45 +1,9 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zipcode" xml:"zipcode"`
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-
-	customers := []Customer{
-		{Name: "John Doe", City: "New York", Zipcode: "110075"},
-		{Name: "Wolfgang Ritter", City: "Rittersberg", Zipcode: "340978"},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
-	}
-
-}
+import "github.com/jrovieri/gobanking/app"
 
 func main() {
 
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	app.Start()
 
 }
