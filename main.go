a5b30a73eb06659965a2e415e5572cb23e8be62b
Jonathan Rovieri <jrovieri@gmail.com>
2025-07-07 19:33:20 -0300

    xml enconding added

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zipcode"`
+	Name    string `json:"full_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zipcode" xml:"zipcode"`
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
@@ -18,11 +19,20 @@ func greet(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+
 	customers := []Customer{
 		{Name: "John Doe", City: "New York", Zipcode: "110075"},
+		{Name: "Wolfgang Ritter", City: "Rittersberg", Zipcode: "340978"},
 	}
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+
 }
 
 func main() {
